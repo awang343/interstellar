97b00eb7786535221c8fc17c47de8a13c8d7cc3b
Alan W <awangc31s@gmail.com>
2025-12-09 19:14:13 -0500

    LSDKJF

--- src/lighting.cpp
+++ src/lighting.cpp
@@ -31,40 +31,39 @@ static inline float calc_falloff(const float &inner, const float &outer, const f
 glm::vec3 shadePixel(const Hit &hit, const ImageData &texture, const BumpMap &bump_map,
                      const std::vector<SceneLightData> &lights)
 {
-    static const float ka = 1.f; // HARDCODED
+    static const float ka = 0.f; // HARDCODED
     static const float kd = 1.f; // HARDCODED
     // static const float ks = 1.f; // HARDCODED
     const float shininess = 0.5f;
 
-    const glm::vec3 obj_A = glm::vec3(0.f, 0.5f, 0.f) * ka; // HARDCODED
+    const glm::vec3 obj_A = glm::vec3(1.f, 1.f, 1.f) * ka; // HARDCODED
     glm::vec3 obj_D = glm::vec3(1.f, 1.f, 1.f) * kd;       // HARDCODED
     // const glm::vec3 obj_S = glm::vec3(1.f, 1.f, 1.f) * ks; // HARDCODED
 
-    const glm::vec3 obj_point = glm::normalize(glm::vec3(hit.point) - hit.sphere.center); // point in object space
+    const glm::vec3 obj_point = (glm::vec3(hit.point) - hit.sphere.center) / hit.sphere.radius / 2.f; // point in object space
+    const glm::vec3 obj_normal = glm::normalize(obj_point);
     const float obj_side = hit.point[3];
     // const glm::mat3 transform = glm::inverse(glm::transpose(glm::mat3(hit.shape->ctm)));
 
     const uv uv_map = get_uv(obj_point);
 
     const bool enableBumpMap = false; // HARDCODED
-    const glm::vec3 obj_normal =
+    const glm::vec3 bump_normal =
         enableBumpMap && bump_map.width > 0
-            ? get_bump_normal(bump_map, FilterType::Bilinear, uv_map, 0.01, obj_point)
-            : obj_point;
+            ? get_bump_normal(bump_map, FilterType::Bilinear, uv_map, 0.01, obj_normal)
+            : obj_normal;
     const glm::vec3 N = glm::normalize(/*transform **/ obj_normal);
     // const glm::vec3 V = glm::normalize(world_camera - glm::vec3(intersect));
 
     // Blend obj_D with textures
-    const bool enableTextureMap = false;                    // HARDCODED
-    const FilterType textureFilter = FilterType::Nearest; // HARDCODED
+    const bool enableTextureMap = true;                    // HARDCODED
+    const FilterType textureFilter = FilterType::Bilinear; // HARDCODED
 
     if (enableTextureMap && texture.width > 0)
     {
-        std::cout << "OPEN" << std::endl;
         const float blend = 1.0; // HARDCODED
         const glm::vec3 texture_color = get_texture(texture, textureFilter, uv_map);
         obj_D = obj_D * (1.f - blend) + texture_color * blend;
-        std::cout << "CLOSE" << std::endl;
     }
 
     glm::vec3 pixel_color(obj_A);
@@ -79,7 +78,7 @@ glm::vec3 shadePixel(const Hit &hit, const ImageData &texture, const BumpMap &bu
 
         glm::vec3 phong(0.f);
 
-        const glm::vec3 to_light = calc_light_vec(obj_point, light);
+        const glm::vec3 to_light = calc_light_vec(glm::vec3(hit.point), light);
         const glm::vec3 L_i = glm::normalize(to_light); // Vector from point to light
         const glm::vec3 R = glm::normalize(2.f * glm::dot(L_i, N) * N - L_i);
 
@@ -119,12 +118,12 @@ glm::vec3 shadePixel(const Hit &hit, const ImageData &texture, const BumpMap &bu
         bool directional = light.type == LightType::LIGHT_DIRECTIONAL;
 
         // Attenuation effect
-        if (!directional && light_intensity > 1e-6f)
-        {
-            const float dist = glm::distance(glm::vec3(obj_point), glm::vec3(light.pos));
-            const float mult = 1.f / glm::dot(light.function, glm::vec3(1.f, dist, dist * dist));
-            light_intensity *= std::min(1.f, mult);
-        }
+        // if (!directional && light_intensity > 1e-6f)
+        // {
+        //     const float dist = glm::distance(glm::vec3(obj_point), glm::vec3(light.pos));
+        //     const float mult = 1.f / glm::dot(light.function, glm::vec3(1.f, dist, dist * dist));
+        //     light_intensity *= std::min(1.f, mult);
+        // }
 
         pixel_color += phong * light_intensity;
     }
